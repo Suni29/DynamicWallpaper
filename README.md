217951c57c9667f12e20f3e2a1ffec0304fc94e5
Suni29 <71086259+Suni29@users.noreply.github.com>
2023-03-04 16:03:15 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Dynamic Wallpaper (For only GNOME right now)
+## DW is another smaller project I created, it has a clock and weather information. The wallpaper updates every minute and mainly uses [Tabliss](https://web.tabliss.io/) (a chrome and firefox extension). Also, it updates the background image every 5 minutes from [Unsplash](https://unsplash.com).
+![Image](./README.png)
+# Configuration
+```json
+{
+    // Set this to your monitor's width and height.
+    "width": 2560,
+    "height": 1080,
+    // Visual settings
+    "backgroundOpacity": 0.5,
+    "timeWidgetFontSize": 36,
+    "weatherWidgetFontSize": 56,
+    // Weather location (I recommend testing the location it on web.tabliss.io first then put it in this config)
+    "weatherLocation": "Budapest",
+    // Full location for an offline wallpaper.
+    "offlineWallpaperPath": "/home/suni29/Pictures/Wallpapers/SurfacePro/dark-mountain-valley-wheat-field.jpg"
+}
+```
+# Autostart
+### To autostart DW use [PM2](https://pm2.keymetrics.io/), here is a guide on how to do it:
+```bash
+$ pm2 startup
+```
+### Follow the instructions from the output then run this 2 commands:
+```bash
+$ pm2 start index.js --name DynamicWallpaper
+$ pm2 save
+```
+# Features
+- ### [x] Dynamic clock and weather widget.
+- ### [x] Dynamic wallpaper change.
+- ### [x] Offline checking ![BETA](./BETA.svg)
+# Known bugs
+- ### Offline wallpaper gets overwritten.
+- ### If the program is started offline then the background image will be black for 1-2 updates.
+# Credits
+### [Tabliss](https://tabliss.io/) and [Unsplash](https://unsplash.com).
---
+++ index.js
@@ -0,0 +1,161 @@
+import puppeteer from 'puppeteer';
+import wait from 'wait';
+import { execSync } from 'child_process';
+import dns from 'dns';
+import { readFileSync } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+let browser;
+let page;
+let initDone = false;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const config = JSON.parse(readFileSync('./config.json', 'utf-8'));
+
+const init = async () => {
+  browser = await puppeteer.launch({ headless: true });
+  page = await browser.newPage();
+
+  await page.goto('https://web.tabliss.io/', {waitUntil: "domcontentloaded"});
+
+  // Set screen size
+  await page.setViewport({width: config.width || 2560, height: config.height || 1080});
+
+  // Add custom style
+  await page.addStyleTag({ path: 'custom.css' });
+
+  // Open settings
+  let sel = '.Overlay > a:nth-child(1)';
+  await page.waitForSelector(sel);
+  await page.click('.Overlay > a:nth-child(1)')
+
+  // Set to every 5 minutes
+  sel = '.UnsplashSettings > label:nth-child(1) > select:nth-child(2)';
+  await page.waitForSelector(sel);
+  await page.select('.UnsplashSettings > label:nth-child(1) > select:nth-child(2)', '300')
+
+  // Set topic
+  sel = '.UnsplashSettings > label:nth-child(3) > input:nth-child(1)';
+  await page.waitForSelector(sel);
+  await page.click('.UnsplashSettings > label:nth-child(3) > input:nth-child(1)')
+
+  // Set topic to wallpapers
+  sel = '.UnsplashSettings > label:nth-child(6) > select:nth-child(1)';
+  await page.waitForSelector(sel);
+  await page.select('.UnsplashSettings > label:nth-child(6) > select:nth-child(1)', 'bo8jQKTaE0Y')
+
+  // Open display settings for background
+  sel = 'div.Widget:nth-child(3) > p:nth-child(3) > a:nth-child(1)';
+  await page.waitForSelector(sel);
+  await page.click('div.Widget:nth-child(3) > p:nth-child(3) > a:nth-child(1)')
+
+  // Set Luminosity
+  sel = '.image';
+  await page.waitForSelector(sel);
+  await page.evaluate(() => {
+    const selector = '.image';
+    const element = document.querySelector(selector);
+    element.style.opacity = `${config.backgroundOpacity ? config.backgroundOpacity.toString() : 0.5}`;
+  });
+
+  // Delete greeting
+  sel = 'fieldset.Widget:nth-child(4) > div:nth-child(1) > button:nth-child(1)';
+  await page.waitForSelector(sel);
+  await page.click('fieldset.Widget:nth-child(4) > div:nth-child(1) > button:nth-child(1)')
+
+  // Add weather
+  sel = '.plane > div:nth-child(3) > label:nth-child(2) > select:nth-child(1)';
+  await page.waitForSelector(sel);
+  await page.select('.plane > div:nth-child(3) > label:nth-child(2) > select:nth-child(1)', 'widget/weather')
+
+  // Edit weather 
+  sel = 'fieldset.Widget:nth-child(4) > div:nth-child(1) > button:nth-child(2)';
+  await page.waitForSelector(sel);
+  await page.click('fieldset.Widget:nth-child(4) > div:nth-child(1) > button:nth-child(2)')
+
+  // Set location for weather
+  sel = '#LocationInput__query';
+  await page.waitForSelector(sel);
+  await page.type('#LocationInput__query', config.weatherLocation || "Budapest");
+  // Submit it
+  sel = 'button.button--primary:nth-child(4)';
+  await page.waitForSelector(sel);
+  await page.click('button.button--primary:nth-child(4)')
+  // Set font size
+  await page.evaluate(() => {
+    const selector = 'div.Widget:nth-child(2)';
+    const style = 'font-size: ' + (config.weatherWidgetFontSize || 56) + 'px; font-family: \'Ubuntu\', sans-serif;';
+    const element = document.querySelector(selector);
+    element.style.cssText += style;
+  });
+
+  // Set font size for time
+  await page.evaluate(() => {
+    const selector = 'div.Widget:nth-child(1)';
+    const style = 'font-family: \'Ubuntu\', sans-serif; font-size: ' + (config.timeWidgetFontSize || 36) + 'px;';
+    const element = document.querySelector(selector);
+    element.style.cssText += style;
+  });
+  
+  // Edit time
+  sel = 'fieldset.Widget:nth-child(3) > div:nth-child(1) > button:nth-child(2)';
+  await page.waitForSelector(sel);
+  await page.click('fieldset.Widget:nth-child(3) > div:nth-child(1) > button:nth-child(2)')
+  // Set 12hr format 
+  sel = '.TimeSettings > label:nth-child(4) > input:nth-child(1)';
+  await page.waitForSelector(sel);
+  await page.click('.TimeSettings > label:nth-child(4) > input:nth-child(1)')
+
+  // Exit settings
+  sel = 'a.fullscreen';
+  await page.waitForSelector(sel);
+  await page.click('a.fullscreen');
+
+  initDone = true;
+}
+
+const takeScreenshot = async () => {
+  // Wait for image to load
+  await wait(1000);
+
+  await page.screenshot({ path: 'screenshot.png' });
+
+  await execSync('gsettings set org.gnome.desktop.background picture-uri-dark file://' + __dirname + '/screenshot.png')
+};
+
+async function scheduleAtStartOfMinute() {
+  const now = new Date();
+  const nextMinute = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 1, 0, 0);
+  const delay = nextMinute - now;
+  console.log('Next update will be in ' + delay + 'ms.')
+  let p;
+  try {
+    p = await lookupPromise();
+  } catch(err) {
+    p = false;
+  }
+  if(!p) { await execSync('gsettings set org.gnome.desktop.background picture-uri-dark file://' + config.offlineWallpaperPath); console.log('Can\'t reach start page, aborting.') }
+  setTimeout(async () => {
+    if(!initDone && p) await init();
+    if(p) takeScreenshot();
+    scheduleAtStartOfMinute();
+  }, delay);
+}
+
+async function lookupPromise() {
+  return new Promise((resolve, reject) => {
+      dns.lookup("web.tabliss.io", (err, address, family) => {
+          if(err) reject(err);
+          resolve(true);
+      });
+ });
+};
+
+(async () => {
+  try {
+    await lookupPromise();
+    await init();
+  } catch(err) {}
+  await scheduleAtStartOfMinute();
+})();
